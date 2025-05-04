c0470c950887991bf14636dbe1c3f14ddb4db967
Claudio Campos <claudio@imageima.com>
2025-05-04 09:40:11 -0400

    Initialize C# console app with greeting and user input

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Console.WriteLine("Welcome to your first C# app!");
+Console.Write("What is your name? ");
+string? name = Console.ReadLine();
+
+Console.WriteLine($"Hello, {name}! You're now learning .NET!");
